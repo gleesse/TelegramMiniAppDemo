0361678c28b0e26f449bd7c613818f699d42a8cc
gleesse <78236116+gleesse@users.noreply.github.com>
2024-05-24 19:35:46 -0400

    Add files via upload

---
+++ mini-app.js
@@ -0,0 +1,24 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('paymentForm');
+    form.addEventListener('submit', handleFormSubmit);
+});
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const receiverName = document.getElementById('receiverName').value;
+    const bankAccount = document.getElementById('bankAccount').value;
+    const amount = document.getElementById('amount').value;
+    const note = document.getElementById('note').value;
+
+    const userInfo = Telegram.WebApp.initData || '';
+    const formData = {
+        receiverName,
+        bankAccount,
+        amount,
+        note,
+        userInfo
+    };
+
+    Telegram.WebApp.sendData(JSON.stringify(formData));
+}
